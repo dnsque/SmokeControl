9d891eeea178fb9f856cc3008b3b29ef542f1054
dnsque <dinia000@gmail.com>
2025-04-15 18:55:49 +0300

    apikey hide

--- src/firebase.ts
+++ src/firebase.ts
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCjgbQHCpq4RaDQUJu1ph7d7D3Og8pY6C4",
-  authDomain: "smokecontrol-84a48.firebaseapp.com",
-  projectId: "smokecontrol-84a48",
-  storageBucket: "smokecontrol-84a48.firebasestorage.app",
-  messagingSenderId: "605349137238",
-  appId: "1:605349137238:web:0cc9b6862b313d556fe4f7",
-  measurementId: "G-TB9XWQKYMR"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
